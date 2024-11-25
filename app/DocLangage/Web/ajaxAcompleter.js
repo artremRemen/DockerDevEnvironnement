6996f528c72cf1be5827af7a269d702dd769fed8
artremRemen <arthuremen@gmail.com>
2024-11-25 18:45:38 +0100

    :arrow_up: dependancy update

--- app/DocLangage/Web/ajaxAcompleter.js
+++
@@ -1,6 +0,0 @@
-var nomvar = new XMLHttpRequest()       //activer ajax
-
-
-httpRequest.open('GET','/secur/require/page.html', true)                 //ouvrir une page sur la page actuel('methode(GET ou POST)', 'URL de destination', 'faire la requete de facon asyncrone')
-
-httpRequest.send()  //envois les infos
